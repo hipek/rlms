bbd5c81912a72c5a9ed01de0755db0234514b039
Jacek Hiszpanski <j.hiszpanski@gmail.com>
2011-01-26 22:41:56 +0100

    updated migration

--- db/migrate/20110126213413_add_type_to_service.rb
+++ db/migrate/20110126213413_add_type_to_service.rb
@@ -1,6 +1,11 @@
 class AddTypeToService < ActiveRecord::Migration
   def self.up
     add_column :services, :type, :string, :limit => 32
+    %w(iptables iptables-save iptables-save iptables-restore ifconfig arp).each do |n|
+      s = Service.find_or_create_by_name(:name => n)
+      s.send :write_attribute, :type, 'Router::Service::Simple'
+      s.save
+    end
   end
 
   def self.down
