a7fcdb60b681709db5b9628abc3810c67f83eef4
Jacek Hiszpanski <j.hiszpanski@gmail.com>
2011-01-27 23:27:34 +0100

    updated main settings

--- app/helpers/router/base_helper.rb
+++ app/helpers/router/base_helper.rb
@@ -2,4 +2,16 @@ module Router::BaseHelper
   def all_protocols
     %w"TCP UDP ALL".map{|pro| [pro, pro.downcase]}
   end
+  
+  def ip_configs
+    %w"auto manually".map{|c| [_(c.camelcase), c]}
+  end
+  
+  def interfaces
+    @interfaces ||= begin
+      infs = ShellCommand.read_interfaces
+      infs = %w"eth0 eth1" if infs.blank?
+      infs
+    end
+  end
 end
--- app/models/router/main.rb
+++ app/models/router/main.rb
@@ -1,13 +1,17 @@
 class Router::Main < Router::BaseSetting
   define_fields(
+    :int_config => 'manually',
+    :ext_config => 'auto',
     :int_inf => 'eth0',
     :ext_inf => 'eth1',
     :int_ip => '',
     :int_mask => '',
     :int_gateway => '',
-    :ext_ip => 'auto',
+    :ext_ip => '',
     :ext_mask => '',
-    :ext_gateway => ''
+    :ext_gateway => '',
+    :dns_server1 => '',
+    :dns_server2 => ''
   )
   class <<self
     def instance
