da6960a66e23e933ef38820185130b8996a9608d
Jacek Hiszpanski <j.hiszpanski@gmail.com>
2011-01-28 22:49:52 +0100

    updated interfaces template

--- app/models/router/main.rb
+++ app/models/router/main.rb
@@ -4,13 +4,27 @@ class Router::Main < Router::BaseSetting
     :dns_server2 => ''
   )
 
-  has_many :interfaces, :class_name => 'Router::Interface', :foreign_key => 'parent_id'
+  has_many :interfaces, :class_name => 'Router::Interface', :foreign_key => 'parent_id' do
+    def find_by_net_type t
+      select{|a| a.net_type == t}.first
+    end
+  end
 
   class <<self
     def instance
       first || new
     end
   end
+
+  def ext_inf
+    interfaces.find_by_net_type('ext')
+  end
+
+  def int_inf
+    interfaces.find_by_net_type('int')
+  end
+
+  protected
 
   def interfaces_attributes= attrs
     self.interfaces = attrs.map{ |key, att|
