bd0fd86037660437e413f3e7632e5794000c0a5e
Jacek Hiszpanski <j.hiszpanski@gmail.com>
2011-02-06 00:34:53 +0100

    added port ranges

--- app/models/router/rule/forward_port.rb
+++ app/models/router/rule/forward_port.rb
@@ -1,3 +1,4 @@
 class Router::Rule::ForwardPort < ActiveRecord::Base
+  include ::Router::Rule::OpenPort::Shared
   belongs_to :computer, :class_name => 'Router::Computer', :foreign_key => :computer_id
 end
--- app/models/router/rule/open_port.rb
+++ app/models/router/rule/open_port.rb
@@ -1,2 +1,19 @@
 class Router::Rule::OpenPort < ActiveRecord::Base
+  module Shared
+    def self.included klass
+      klass.class_eval do
+        validates_presence_of :port, :protocol
+      end
+    end
+
+    def protocols
+      protocol.downcase == 'all' ? %w"tcp udp" : [protocol.downcase]
+    end
+
+    def ports
+      [port.split(',')].flatten.compact
+    end
+  end
+
+  include Shared
 end
