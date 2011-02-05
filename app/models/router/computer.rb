9339cc3a84022727de696960fe47982a2e9413ad
Jacek Hiszpanski <j.hiszpanski@gmail.com>
2011-02-05 21:31:53 +0100

    updated methods for finding computers

--- app/models/router/computer.rb
+++ app/models/router/computer.rb
@@ -6,43 +6,50 @@ class Router::Computer < ActiveRecord::Base
   before_validation :upcase_mac
 
   SORT_BY_IP = "substr( `ip_address` , '.', -1 ) + 0"
-  
-  def upcase_mac
-    return unless mac_address.present?
-    self.mac_address = self.mac_address.gsub(/-|;|,|\./, ':')
-    self.mac_address = self.mac_address.to_s.upcase
-  end
 
-  def self.all_for_dhcpd
-    find(:all, :order => SORT_BY_IP) - [find_router]
-  end
-  
-  def self.find_router
-    find_by_name('router')
-  end
+  scope :without_router, where("name != 'router'")
+  scope :sorted_by_ip, order(SORT_BY_IP)
 
-  def self.next_ip
-    ip = all_for_dhcpd.last.try:ip_address
-    return ip if ip.blank?
-    elements = ip.split('.')
-    elements[elements.length - 1] = (elements.last.to_i + 1).to_s
-    elements.join('.')
-  end
-
-  def self.all_online
-    lines = ShellCommand.arp.delete('(/)/[/]').gsub('?','unknown').strip.split(/\n/)
-    computers = []
-    lines.each do |line|
-      parts = line.split(' ')
-      computers << new(:name => parts.first, :ip_address => parts[1], :mac_address => parts[3])
+  class <<self
+    def all_for_dhcpd
+      without_router.sorted_by_ip.all
     end
-    computers
+
+    def allow_computers
+      without_router.sorted_by_ip.all
+    end
+
+    def blocked_computers
+      []
+    end
+
+    def find_router
+      find_by_name('router')
+    end
+
+    def next_ip
+      ip = all_for_dhcpd.last.try:ip_address
+      return ip if ip.blank?
+      elements = ip.split('.')
+      elements[elements.length - 1] = (elements.last.to_i + 1).to_s
+      elements.join('.')
+    end
+
+    def all_online
+      lines = ShellCommand.arp.delete('(/)/[/]').gsub('?','unknown').strip.split(/\n/)
+      computers = []
+      lines.each do |line|
+        parts = line.split(' ')
+        computers << new(:name => parts.first, :ip_address => parts[1], :mac_address => parts[3])
+      end
+      computers
+    end
   end
-  
+
   def block firewall
     FwRule.block_computer self, firewall
   end
-  
+
   def pass firewall
     FwRule.pass_computer self, firewall
   end
@@ -50,8 +57,14 @@ class Router::Computer < ActiveRecord::Base
   def valid_name
     name.latinize.downcase.gsub(' ', '_').gsub('/','_').gsub('\\','_')
   end
-  
+
   def router?
     name.match(/ROUTER/)
+  end
+
+  def upcase_mac
+    return unless mac_address.present?
+    self.mac_address = self.mac_address.gsub(/-|;|,|\./, ':')
+    self.mac_address = self.mac_address.to_s.upcase
   end
 end
