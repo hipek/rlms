e446d4be2e7a991532a837f3778bf83014a0b790
Jacek Hiszpanski <j.hiszpanski@gmail.com>
2011-02-06 12:19:19 +0100

    removed old helpers with specs

--- app/helpers/torrent/items_helper.rb
+++ app/helpers/torrent/items_helper.rb
@@ -1,2 +1,9 @@
 module Torrent::ItemsHelper
+  def tr_td_row name, value, options = { }
+    content_tag :tr, content_tag(:td, name) + content_tag(:td, value), options
+  end
+  
+  def tr_td_row_spacer
+    tr_td_row '&nbsp;', '&nbsp;'
+  end
 end
