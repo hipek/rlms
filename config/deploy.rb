8aa526af165b4622e1e6f3ea4d9754c209f27a5e
Jacek Hiszpanski <j.hiszpanski@gmail.com>
2014-08-23 15:26:16 +0200

    Fix links for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,10 +23,10 @@ set :deploy_to, '/home/deploy/rlms4'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/rtorrent.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/rtorrent.yml config/secrets.yml db/production.sqlite3}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
