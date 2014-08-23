fb306f777c140540c63c4a39a9f642be62cf7956
Jacek Hiszpanski <j.hiszpanski@gmail.com>
2014-08-23 19:16:18 +0200

    Assets update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,6 +34,15 @@ Rlms::Application.configure do
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
+
+  # Generate digests for assets URLs.
+  config.assets.digest = true
+
+  # Version of your assets, change this if you want to expire all your assets.
+  config.assets.version = '1.0'
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
