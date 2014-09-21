2ece23a08e494dc9a8931cb606e8f2ac27aa828d
Jacek Hiszpanski <j.hiszpanski@gmail.com>
2014-09-21 18:37:02 +0200

    Update torrent button

--- app/helpers/buttons_helper.rb
+++ app/helpers/buttons_helper.rb
@@ -3,4 +3,8 @@ module ButtonsHelper
     opts[:class] ||= 'btn btn-primary'
     super(name, opts)
   end
+
+  def small_btn_tag(name, opts = {})
+    submit_tag(name, class: 'btn btn-primary btn-xs')
+  end
 end
