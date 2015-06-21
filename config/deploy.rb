a7e62a9750aadad4160c95777cced91cbf74aa86
Jacek Hiszpanski <j.hiszpanski@gmail.com>
2015-06-21 16:08:34 +0200

    Add settings yml to deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, '/home/deploy/rlms4'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/rtorrent.yml config/secrets.yml db/production.sqlite3}
+set :linked_files, %w{config/database.yml config/rtorrent.yml config/secrets.yml config/settings.yml db/production.sqlite3}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
