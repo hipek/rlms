7ae94d34bcd5cd9274eabf96dd5b4410f3c9f65e
Jacek Hiszpanski <j.hiszpanski@gmail.com>
2016-01-02 19:34:17 +0100

    Cleanup lines

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -6,28 +6,28 @@ describe "Group" do
     before(:each) do
       @group = Group.new(:name => "Hunters")
     end
-    
+
     it "should be valid" do
       @group.should be_valid
     end
-    
+
     it "should have a unique name" do
       @group.save
       @group2 = Group.new(:name => "Hunters")
       @group2.should_not be_valid
     end
   end
-  
+
   describe "associations" do
     fixtures :groups, :group_memberships
-        
+
     it "should get groups correctly" do
       groups(:publishers).groups.size.should == 2
       arr = []
       arr << groups(:customers)
       arr << groups(:company)
       groups(:publishers).groups.should == arr
-      
+
       groups(:admins).groups.size.should == 1
       arr = []
       arr << groups(:company)
